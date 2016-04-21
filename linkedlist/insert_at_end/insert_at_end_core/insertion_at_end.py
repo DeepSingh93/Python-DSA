de3cbed852424c9d7d889e6082be5f5a5c04b624
Deep Singh <deep_prakash_singh@live.com>
2016-04-21 08:21:25 +0000

    updating linkedlist folders

--- linkedlist/insert_at_end/insert_at_end_core/insertion_at_end.py
+++ linkedlist/insert_at_end/insert_at_end_core/insertion_at_end.py
@@ -1,5 +1,6 @@
-# add necessary directories to import
+# add necessary directories to import (change your import directory)
 import sys;
+# IMPORTANT : Change your import directory else this will not work
 sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_end');
 
 #import node class
@@ -10,3 +11,26 @@ class insert_at_end:
         print("#created empty linked list")
         self.head = None;
         self.tail = None;
+        
+    def add(self, data):
+        print("creating node object")
+        nodeObject = Node(data)
+        print("#Checking head");
+        if self.head == None:
+            print("#Head is none, adding first element");
+            self.head = self.tail = nodeObject;
+        else:
+            print("#Head is not none, adding an element to the end of list")
+            self.tail.next = nodeObject;
+            self.tail = nodeObject;
+        print("#----- ELEMENT SUCCESSFULLY ADDED ELEMENT : "+data+" -----")
+    
+    def printLinkedList(self):
+        print("#Get head position and save it in local variable");
+        getHead = self.head;
+        print("#--- START OF PRINTING OF LINKEDLIST ---")
+        while(getHead != None):
+            print(getHead.data)
+            getHead = getHead.next;
+        print("#--- END OF PRINTING OF LINKEDLIST ---")
+        
--- linkedlist/run_this_file.py
+++ linkedlist/run_this_file.py
@@ -1,5 +1,13 @@
 # add necessary directories to import
 import sys;
+#inserting at the end using linkedlist in python (change your import directory)
+# IMPORTANT : Change your import directory else this will not work
 sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_end/insert_at_end_core');
 from insertion_at_end import insert_at_end;
 testLinkedlist = insert_at_end();
+testLinkedlist.add("sample element 1");
+testLinkedlist.add("sample element 2");
+testLinkedlist.add("sample element 3");
+testLinkedlist.add("sample element 4");
+testLinkedlist.add("sample element 5");
+testLinkedlist.printLinkedList();
