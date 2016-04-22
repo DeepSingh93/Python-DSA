82f9085cf943d54eadadfaeac4f031d2938cd0d1
Deep Singh <deep_prakash_singh@live.com>
2016-04-22 09:01:26 +0000

    updating run_this_file.py

--- linkedlist/run_this_file.py
+++ linkedlist/run_this_file.py
@@ -3,69 +3,69 @@ import sys;
 #inserting at the end using linkedlist in python (change your import directory)
 # IMPORTANT : Change your import directory else this will not work
 
-# # Program using python for adding element at the end
-# sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_end/insert_at_end_core');
-# from insertion_at_end import insert_at_end;
-# testLinkedlist = insert_at_end();
-# testLinkedlist.add("sample element 1");
-# testLinkedlist.add("sample element 2");
-# testLinkedlist.add("sample element 3");
-# testLinkedlist.add("sample element 4");
-# testLinkedlist.add("sample element 5");
-# testLinkedlist.printLinkedList();
+# Program using python for adding element at the end
+sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_end/insert_at_end_core');
+from insertion_at_end import insert_at_end;
+testLinkedlist = insert_at_end();
+testLinkedlist.add("sample element 1");
+testLinkedlist.add("sample element 2");
+testLinkedlist.add("sample element 3");
+testLinkedlist.add("sample element 4");
+testLinkedlist.add("sample element 5");
+testLinkedlist.printLinkedList();
 
-# # Program in python to add element after an element
-# sys.path.append('/home/ubuntu/workspace/linkedlist/insert_after_an_element/insert_after_an_element_core');
-# from insert_after_an_element import insert_after_an_element_class
-# testLinkedList = insert_after_an_element_class();
-# testLinkedList.add("Sample Data 1")
-# testLinkedList.add("Sample Data 2")
-# testLinkedList.add("Sample Data 3")
-# testLinkedList.add("Sample Data 4")
-# testLinkedList.printLinkedList();
-# testLinkedList.addElementAfterAnElement('Sample Data 2', 'added data')
-# testLinkedList.printLinkedList();
+# Program in python to add element after an element
+sys.path.append('/home/ubuntu/workspace/linkedlist/insert_after_an_element/insert_after_an_element_core');
+from insert_after_an_element import insert_after_an_element_class
+testLinkedList = insert_after_an_element_class();
+testLinkedList.add("Sample Data 1")
+testLinkedList.add("Sample Data 2")
+testLinkedList.add("Sample Data 3")
+testLinkedList.add("Sample Data 4")
+testLinkedList.printLinkedList();
+testLinkedList.addElementAfterAnElement('Sample Data 2', 'added data')
+testLinkedList.printLinkedList();
 
-# # Program in python to add element before an element
-# sys.path.append('/home/ubuntu/workspace/linkedlist/insert_before_an_element/insert_before_an_element_core');
-# from insert_before_an_element import insert_before_an_element_class
-# testLinkedList = insert_before_an_element_class();
-# testLinkedList.add("Sample Data 1")
-# testLinkedList.add("Sample Data 2")
-# testLinkedList.add("Sample Data 3")
-# testLinkedList.add("Sample Data 4")
-# testLinkedList.printLinkedList();
-# testLinkedList.addBefore('Sample Data 2', 'added data')
-# testLinkedList.printLinkedList();
+# Program in python to add element before an element
+sys.path.append('/home/ubuntu/workspace/linkedlist/insert_before_an_element/insert_before_an_element_core');
+from insert_before_an_element import insert_before_an_element_class
+testLinkedList = insert_before_an_element_class();
+testLinkedList.add("Sample Data 1")
+testLinkedList.add("Sample Data 2")
+testLinkedList.add("Sample Data 3")
+testLinkedList.add("Sample Data 4")
+testLinkedList.printLinkedList();
+testLinkedList.addBefore('Sample Data 2', 'added data')
+testLinkedList.printLinkedList();
 
-# #Program in python to insert elemnet in the begining
-# sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_begining/insert_at_begining_core');
-# from insert_at_begining import insert_at_begining_class
-# testLinkedList = insert_at_begining_class();
-# testLinkedList.add("Sample Data 1")
-# testLinkedList.add("Sample Data 2")
-# testLinkedList.add("Sample Data 3")
-# testLinkedList.add("Sample Data 4")
-# testLinkedList.printLinkedList();
+#Program in python to insert elemnet in the begining
+sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_begining/insert_at_begining_core');
+from insert_at_begining import insert_at_begining_class
+testLinkedList = insert_at_begining_class();
+testLinkedList.add("Sample Data 1")
+testLinkedList.add("Sample Data 2")
+testLinkedList.add("Sample Data 3")
+testLinkedList.add("Sample Data 4")
+testLinkedList.printLinkedList();
 
 
-# # Program in python to delete an elemnet from end
-# sys.path.append('/home/ubuntu/workspace/linkedlist/delete_element_end/delete_element_end_core');
-# from delete_element_end import delete_element_end_class
-# testLinkedList = delete_element_end_class();
-# testLinkedList.add("Sample Data 1")
-# testLinkedList.add("Sample Data 2")
-# testLinkedList.add("Sample Data 3")
-# testLinkedList.add("Sample Data 4")
-# testLinkedList.printLinkedList();
-# testLinkedList.removeElementEnd()
-# testLinkedList.printLinkedList();
-# testLinkedList.removeElementEnd()
-# testLinkedList.printLinkedList();
-# testLinkedList.removeElementEnd()
-# testLinkedList.printLinkedList();
-# testLinkedList.removeElementEnd()
-# testLinkedList.printLinkedList();
+# Program in python to delete an elemnet from end
+sys.path.append('/home/ubuntu/workspace/linkedlist/delete_element_end/delete_element_end_core');
+from delete_element_end import delete_element_end_class
+testLinkedList = delete_element_end_class();
+testLinkedList.add("Sample Data 1")
+testLinkedList.add("Sample Data 2")
+testLinkedList.add("Sample Data 3")
+testLinkedList.add("Sample Data 4")
+testLinkedList.printLinkedList();
+testLinkedList.removeElementEnd()
+testLinkedList.printLinkedList();
+testLinkedList.removeElementEnd()
+testLinkedList.printLinkedList();
+testLinkedList.removeElementEnd()
+testLinkedList.printLinkedList();
+testLinkedList.removeElementEnd()
+testLinkedList.printLinkedList();
 
 # Program in python to delete an elemnet from begining
 sys.path.append('/home/ubuntu/workspace/linkedlist/delete_element_begining/delete_element_begining_core');
