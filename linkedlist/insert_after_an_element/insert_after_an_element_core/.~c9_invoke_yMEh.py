c01104b6896fb73e463b4d00a726cc892addbce1
Deep Singh <deep_prakash_singh@live.com>
2016-04-22 05:10:42 +0000

    updating linkedlist

---
+++ linkedlist/insert_after_an_element/insert_after_an_element_core/.~c9_invoke_yMEh.py
@@ -0,0 +1,57 @@
+# add necessary directories to import (change your import directory)
+import sys;
+# IMPORTANT : Change your import directory else this will not work
+sys.path.append('/home/ubuntu/workspace/linkedlist/insert_after_an_element');
+
+#import node class
+from Node.Node_Core import Node
+
+class insert_after_an_element_class():
+    def __init__(self):
+        print("#Creating a linked list with empty head and tail")
+        self.head = None;
+        self.tail = None;
+    
+    def add(self, data):
+        print("#create node object")
+        nodeObject = Node(data);
+        print("#check head and tail")
+        if self.head == None:
+            print("#Head is none, adding first element")
+            self.head = self.tail = nodeObject
+        else:
+            print("#Head is not null, appending an element")
+            self.tail.next = nodeObject;
+            self.tail = nodeObject;
+        print("#--- Element added : " + data)
+    
+    def addElementAfterAnElement(self, afterElement, element):
+        dataFound = False;
+        getHead = self.head
+        print("#Finding element")
+        while(self.head != None):
+            if self.head.data == afterElement:
+                dataFound = True
+                print("#Element FOUND !")
+                print("#Creating node object")
+                nodeObject = Node(element);
+                print("#Adding Element")
+                tempSelfHeadNext = self.head.next
+                self.head.next = nodeObject
+                nodeObject.next = tempSelfHeadNext
+                print("#--- ELEMENT ADDED ---")
+            self.head = self.head.next
+            []
+        if dataFound == False:
+            print("element not found")
+        self.head = getHead
+    
+    def printLinkedList(self):
+        print("#Getting head position")
+        getHead = self.head;
+        print("#---- Start of printing of linkedlist ----")
+        while(getHead != None):
+            print(getHead.data)
+            getHead = getHead.next
+        print("#---- End of printing of linkedlist ----")
+        
---
+++ linkedlist/insert_before_an_element/insert_before_an_element_core/insert_before_an_element.py
@@ -0,0 +1,60 @@
+# add necessary directories to import (change your import directory)
+import sys;
+# IMPORTANT : Change your import directory else this will not work
+sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_end');
+
+from Node.Node_Core import Node
+
+class insert_before_an_element_class:
+    def __init__(self):
+        self.head = None;
+        self.tail = None;
+    
+    def add(self, data):
+        print("#Begin addition of element")
+        print("#Creating node object")
+        nodeObject = Node(data)
+        print("#Check whether head is null or not")
+        if(self.head == None):
+            print("#Head is null, adding first element")
+            self.head = self.tail = nodeObject
+        else:
+            print("#Head is not null, appending element")
+            self.tail.next = nodeObject
+            self.tail = nodeObject
+        print("#Finish addition of element")
+    
+    def addBefore(self, elementBefore, element):
+        print("#Initially keeping elementFound = False")
+        elementFound = False
+        print("Save the head position")
+        tempHead = self.head
+        while(self.head.next != None):
+            if self.head.next.data == elementBefore :
+                
+                elementFound = True
+                print("#Getting current element position")
+                getCurrElement = self.head
+                print("#Getting next head position")
+                getnextElement = self.head.next
+                print("#Creating node object")
+                nodeObject = Node(element)
+                
+                self.head.next = nodeObject
+                nodeObject.next = getnextElement
+                
+                self.head = nodeObject.next
+                
+            self.head = self.head.next
+        if elementFound == False:
+            print("Element Not Found")
+        self.head = tempHead
+    
+    def printLinkedList(self):
+        print("#Getting head position")
+        getHead = self.head;
+        print("#---- Start of printing of linkedlist ----")
+        while(getHead != None):
+            print(getHead.data)
+            getHead = getHead.next
+        print("#---- End of printing of linkedlist ----")
