c92a7cda3778cf9f58f8691f4aa34477b021f2c4
Deep Singh <deep_prakash_singh@live.com>
2016-04-26 06:30:57 +0000

    updating run_this_file.py

--- run_this_file.py
+++ run_this_file.py
@@ -3,115 +3,117 @@ import sys;
 #inserting at the end using linkedlist in python (change your import directory)
 # IMPORTANT : Change your import directory else this will not work
 
-# # Program using python for adding element at the end
-# sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_end/insert_at_end_core');
-# from insertion_at_end import insert_at_end;
-# testLinkedlist = insert_at_end();
-# testLinkedlist.add("sample element 1");
-# testLinkedlist.add("sample element 2");
-# testLinkedlist.add("sample element 3");
-# testLinkedlist.add("sample element 4");
-# testLinkedlist.add("sample element 5");
-# testLinkedlist.printLinkedList();
+# Program using python for adding element at the end
+sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_end/insert_at_end_core');
+from insertion_at_end import insert_at_end;
+testLinkedlist = insert_at_end();
+testLinkedlist.add("sample element 1");
+testLinkedlist.add("sample element 2");
+testLinkedlist.add("sample element 3");
+testLinkedlist.add("sample element 4");
+testLinkedlist.add("sample element 5");
+testLinkedlist.printLinkedList();
 
-# # Program in python to add element after an element
-# sys.path.append('/home/ubuntu/workspace/linkedlist/insert_after_an_element/insert_after_an_element_core');
-# from insert_after_an_element import insert_after_an_element_class
-# testLinkedList = insert_after_an_element_class();
-# testLinkedList.add("Sample Data 1")
-# testLinkedList.add("Sample Data 2")
-# testLinkedList.add("Sample Data 3")
-# testLinkedList.add("Sample Data 4")
-# testLinkedList.printLinkedList();
-# testLinkedList.addElementAfterAnElement('Sample Data 2', 'added data')
-# testLinkedList.printLinkedList();
+# Program in python to add element after an element
+sys.path.append('/home/ubuntu/workspace/linkedlist/insert_after_an_element/insert_after_an_element_core');
+from insert_after_an_element import insert_after_an_element_class
+testLinkedList = insert_after_an_element_class();
+testLinkedList.add("Sample Data 1")
+testLinkedList.add("Sample Data 2")
+testLinkedList.add("Sample Data 3")
+testLinkedList.add("Sample Data 4")
+testLinkedList.printLinkedList();
+testLinkedList.addElementAfterAnElement('Sample Data 2', 'added data')
+testLinkedList.printLinkedList();
 
-# # Program in python to add element before an element
-# sys.path.append('/home/ubuntu/workspace/linkedlist/insert_before_an_element/insert_before_an_element_core');
-# from insert_before_an_element import insert_before_an_element_class
-# testLinkedList = insert_before_an_element_class();
-# testLinkedList.add("Sample Data 1")
-# testLinkedList.add("Sample Data 2")
-# testLinkedList.add("Sample Data 3")
-# testLinkedList.add("Sample Data 4")
-# testLinkedList.printLinkedList();
-# testLinkedList.addBefore('Sample Data 2', 'added data')
-# testLinkedList.printLinkedList();
+# Program in python to add element before an element
+sys.path.append('/home/ubuntu/workspace/linkedlist/insert_before_an_element/insert_before_an_element_core');
+from insert_before_an_element import insert_before_an_element_class
+testLinkedList = insert_before_an_element_class();
+testLinkedList.add("Sample Data 1")
+testLinkedList.add("Sample Data 2")
+testLinkedList.add("Sample Data 3")
+testLinkedList.add("Sample Data 4")
+testLinkedList.printLinkedList();
+testLinkedList.addBefore('Sample Data 2', 'added data')
+testLinkedList.printLinkedList();
 
-# #Program in python to insert elemnet in the begining
-# sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_begining/insert_at_begining_core');
-# from insert_at_begining import insert_at_begining_class
-# testLinkedList = insert_at_begining_class();
-# testLinkedList.add("Sample Data 1")
-# testLinkedList.add("Sample Data 2")
-# testLinkedList.add("Sample Data 3")
-# testLinkedList.add("Sample Data 4")
-# testLinkedList.printLinkedList();
+#Program in python to insert elemnet in the begining
+sys.path.append('/home/ubuntu/workspace/linkedlist/insert_at_begining/insert_at_begining_core');
+from insert_at_begining import insert_at_begining_class
+testLinkedList = insert_at_begining_class();
+testLinkedList.add("Sample Data 1")
+testLinkedList.add("Sample Data 2")
+testLinkedList.add("Sample Data 3")
+testLinkedList.add("Sample Data 4")
+testLinkedList.printLinkedList();
 
 
-# # Program in python to delete an elemnet from end
-# sys.path.append('/home/ubuntu/workspace/linkedlist/delete_element_end/delete_element_end_core');
-# from delete_element_end import delete_element_end_class
-# testLinkedList = delete_element_end_class();
-# testLinkedList.add("Sample Data 1")
-# testLinkedList.add("Sample Data 2")
-# testLinkedList.add("Sample Data 3")
-# testLinkedList.add("Sample Data 4")
-# testLinkedList.printLinkedList();
-# testLinkedList.removeElementEnd()
-# testLinkedList.printLinkedList();
-# testLinkedList.removeElementEnd()
-# testLinkedList.printLinkedList();
-# testLinkedList.removeElementEnd()
-# testLinkedList.printLinkedList();
-# testLinkedList.removeElementEnd()
-# testLinkedList.printLinkedList();
+# Program in python to delete an elemnet from end
+sys.path.append('/home/ubuntu/workspace/linkedlist/delete_element_end/delete_element_end_core');
+from delete_element_end import delete_element_end_class
+testLinkedList = delete_element_end_class();
+testLinkedList.add("Sample Data 1")
+testLinkedList.add("Sample Data 2")
+testLinkedList.add("Sample Data 3")
+testLinkedList.add("Sample Data 4")
+testLinkedList.printLinkedList();
+testLinkedList.removeElementEnd()
+testLinkedList.printLinkedList();
+testLinkedList.removeElementEnd()
+testLinkedList.printLinkedList();
+testLinkedList.removeElementEnd()
+testLinkedList.printLinkedList();
+testLinkedList.removeElementEnd()
+testLinkedList.printLinkedList();
 
-# # Program in python to delete an elemnet from begining
-# sys.path.append('/home/ubuntu/workspace/linkedlist/delete_element_begining/delete_element_begining_core');
-# from delete_element_begining import delete_element_begining_class
-# testLinkedList = delete_element_begining_class();
-# testLinkedList.add("Sample Data 1")
-# testLinkedList.add("Sample Data 2")
-# testLinkedList.add("Sample Data 3")
-# testLinkedList.add("Sample Data 4")
-# testLinkedList.printLinkedList()
-# testLinkedList.delete()
-# testLinkedList.printLinkedList()
-# testLinkedList.delete()
-# testLinkedList.printLinkedList()
-# testLinkedList.delete()
-# testLinkedList.printLinkedList()
+# Program in python to delete an elemnet from begining
+sys.path.append('/home/ubuntu/workspace/linkedlist/delete_element_begining/delete_element_begining_core');
+from delete_element_begining import delete_element_begining_class
+testLinkedList = delete_element_begining_class();
+testLinkedList.add("Sample Data 1")
+testLinkedList.add("Sample Data 2")
+testLinkedList.add("Sample Data 3")
+testLinkedList.add("Sample Data 4")
+testLinkedList.printLinkedList()
+testLinkedList.delete()
+testLinkedList.printLinkedList()
+testLinkedList.delete()
+testLinkedList.printLinkedList()
+testLinkedList.delete()
+testLinkedList.printLinkedList()
 
-# # Program in python to delete an elemnet from middle
-# sys.path.append('/home/ubuntu/workspace/linkedlist/delete_element_middle/delete_element_middle_core');
-# from delete_element_middle import delete_element_middle_class
-# testLinkedList = delete_element_middle_class();
-# testLinkedList.add("Sample Data 1")
-# testLinkedList.add("Sample Data 2")
-# testLinkedList.add("Sample Data 3")
-# testLinkedList.add("Sample Data 4")
-# testLinkedList.add("Sample Data 5")
-# testLinkedList.printLinkedList()
-# testLinkedList.remove("Sample Data 5")
-# testLinkedList.printLinkedList()
-# testLinkedList.remove("Sample Data 2")
-# testLinkedList.printLinkedList()
-# testLinkedList.remove("Sample Data 3")
-# testLinkedList.printLinkedList()
-# testLinkedList.remove("Sample Data 1")
-# testLinkedList.printLinkedList()
+# Program in python to delete an elemnet from middle
+sys.path.append('/home/ubuntu/workspace/linkedlist/delete_element_middle/delete_element_middle_core');
+from delete_element_middle import delete_element_middle_class
+testLinkedList = delete_element_middle_class();
+testLinkedList.add("Sample Data 1")
+testLinkedList.add("Sample Data 2")
+testLinkedList.add("Sample Data 3")
+testLinkedList.add("Sample Data 4")
+testLinkedList.add("Sample Data 5")
+testLinkedList.printLinkedList()
+testLinkedList.remove("Sample Data 5")
+testLinkedList.printLinkedList()
+testLinkedList.remove("Sample Data 2")
+testLinkedList.printLinkedList()
+testLinkedList.remove("Sample Data 3")
+testLinkedList.printLinkedList()
+testLinkedList.remove("Sample Data 1")
+testLinkedList.printLinkedList()
 
-# # Program in python to add element in doubly linkedlist
-# sys.path.append('/home/ubuntu/workspace/doublylinkedlist/insert_at_end/insert_at_end_core');
-# from insertion_at_end import insert_at_end_doublylinkedlist_class
-# testLinkedList = insert_at_end_doublylinkedlist_class()
-# testLinkedList.add("1")
-# testLinkedList.add("2")
-# testLinkedList.add("3")
-# testLinkedList.add("4")
-# testLinkedList.add("5")
-# testLinkedList.printBothSides()
+---- Important : Comment out all the previous code (above) to run the below code
+
+# Program in python to add element in doubly linkedlist
+sys.path.append('/home/ubuntu/workspace/doublylinkedlist/insert_at_end_doubly/insert_at_end_doubly_core');
+from insertion_at_end_doubly import insert_at_end_doublylinkedlist_class
+testLinkedList = insert_at_end_doublylinkedlist_class()
+testLinkedList.add("1")
+testLinkedList.add("2")
+testLinkedList.add("3")
+testLinkedList.add("4")
+testLinkedList.add("5")
+testLinkedList.printBothSides()
 
 # Program in python to add element in doubly linkedlist
 sys.path.append('/home/ubuntu/workspace/doublylinkedlist/reverse_traversal/reverse_traversal_core');
@@ -125,3 +127,30 @@ testLinkedList.add("5")
 testLinkedList.printReverseLinkedList()
 testLinkedList.add("6")
 testLinkedList.printReverseLinkedList()
+
+# Program in python to add element in doubly linkedlist
+sys.path.append('/home/ubuntu/workspace/doublylinkedlist/reverse_linked_list/reverse_linked_list_core');
+from reverse_linked_list import reverse_linked_list_class
+testLinkedList = reverse_linked_list_class()
+testLinkedList.add("1")
+testLinkedList.add("2")
+testLinkedList.add("3")
+testLinkedList.add("4")
+testLinkedList.add("5")
+testLinkedList.reverse_linked_list()
+testLinkedList.printBothSides()
+
+# Program in python to add element in doubly linkedlist
+sys.path.append('/home/ubuntu/workspace/doublylinkedlist/insert_at_beg_doubly/insert_at_beg_doubly_core');
+from insert_at_beg_doubly import insert_at_beg_doubly_class
+testLinkedList = insert_at_beg_doubly_class()
+testLinkedList.add("1")
+testLinkedList.printLinkedList()
+testLinkedList.add("2")
+testLinkedList.printLinkedList()
+testLinkedList.add("3")
+testLinkedList.printLinkedList()
+testLinkedList.add("4")
+testLinkedList.printLinkedList()
+testLinkedList.add("5")
+testLinkedList.printBothSides()
